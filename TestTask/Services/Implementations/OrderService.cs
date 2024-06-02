15ffd3050afd0367091c8f0bfacbad15e8da88c4
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-06-03 00:24:44 +0300

    test task

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _context;
+
+    public OrderService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Order> GetOrder()
+    {
+        var order = await _context.Orders
+            .Where(o => o.Quantity > 1)
+            .OrderByDescending(o => o.CreatedAt)
+            .FirstOrDefaultAsync() ?? null;
+
+        return order;
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        var orders = await _context.Orders
+            .Where(o => o.User.Status == UserStatus.Active)
+            .OrderBy(o => o.CreatedAt)
+            .ToListAsync();
+
+        return orders;
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetUser()
+    {
+        var user = await _context.Users
+            .Where(u => u.Orders
+                .Any(o => o.CreatedAt.Year == 2003))
+            .OrderByDescending(u => u.Orders
+                .Where(o => o.CreatedAt.Year == 2003)
+                .Sum(o => o.Price * o.Quantity))
+            .FirstOrDefaultAsync() ?? null;
+
+        return user;
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        var users = await _context.Users
+            .Where(u => u.Orders
+                .Any(o => o.CreatedAt.Year == 2010 && o.Status == OrderStatus.Paid))
+            .ToListAsync();
+
+        return users;
+    }
+}
